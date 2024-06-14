94eb212e0eb3ea6f2e1362514890f26d61a30965
ybeth <ybethgonzaga@gmail.com>
2024-06-14 23:51:50 +0800

    Update 02_total_sales_analysis.sql

--- sql files/02_total_sales_analysis.sql
+++ sql files/02_total_sales_analysis.sql
@@ -12,7 +12,7 @@
 -- -- from vid
 -- SELECT SUM(unit_price * transaction_qty) AS total_sales
 -- FROM coffee_shop_sales
--- WHERE MONTH(transaction_date) = 5 -- may
+-- WHERE MONTH(transaction_date) = 5; -- may
 -- -- from me
 -- SELECT DISTINCT DATE_FORMAT(transaction_date, '%Y-%m') 
 -- FROM coffee_shop_sales;
@@ -21,24 +21,38 @@
 -- WHERE DATE_FORMAT(transaction_date, '%Y-%m') = '2023-01';
 
 -- -- • Determine the month-on-month % increase or decrease in sales.
--- Select the month from the transaction_date column
-SELECT 
-    MONTH(transaction_date) AS month, -- number of month
-    -- Calculate the total sales for each month by summing the product of unit_price and transaction_qty
-    ROUND(SUM(unit_price * transaction_qty)) AS total_sales, -- round to whole number
-    -- Calculate the month-over-month percentage increase in total sales
-    (SUM(unit_price * transaction_qty) - 
-		LAG(SUM(unit_price * transaction_qty), 1) OVER (ORDER BY MONTH(transaction_date))) 
-	/ LAG(SUM(unit_price * transaction_qty), 1) OVER (ORDER BY MONTH(transaction_date)) * 100 
-	AS mom_increase_percentage
-FROM coffee_shop_sales
--- Filter the results to include only transactions from April and May
-WHERE 
-    MONTH(transaction_date) IN (4, 5)
--- Group the results by month to aggregate the total sales
-GROUP BY 
-    MONTH(transaction_date)
--- Order the results by month
-ORDER BY 
-    MONTH(transaction_date);
+-- -- Select the month from the transaction_date column
+-- SELECT 
+--     MONTH(transaction_date) AS month, -- number of month
+--     -- Calculate the total sales for each month by summing the product of unit_price and transaction_qty
+--     ROUND(SUM(unit_price * transaction_qty)) AS total_sales, -- round to whole number
+--     -- Calculate the month-over-month percentage increase in total sales
+--     (SUM(unit_price * transaction_qty) - 
+-- 		LAG(SUM(unit_price * transaction_qty), 1) OVER (ORDER BY MONTH(transaction_date))) 
+-- 	/ LAG(SUM(unit_price * transaction_qty), 1) OVER (ORDER BY MONTH(transaction_date)) * 100 
+-- 	AS mom_increase_percentage
+-- FROM coffee_shop_sales
+-- -- Filter the results to include only transactions from April and May
+-- WHERE 
+--     MONTH(transaction_date) IN (4, 5)
+-- -- Group the results by month to aggregate the total sales
+-- GROUP BY 
+--     MONTH(transaction_date)
+-- -- Order the results by month
+-- ORDER BY 
+--     MONTH(transaction_date);
 
+-- -- • Calculate the difference in sales between the selected month and the previous month.
+-- SELECT 
+-- 	MONTH(transaction_date) AS month,
+--     SUM(transaction_qty * unit_price) AS total_sales,
+--     (SUM(transaction_qty * unit_price) - LAG(SUM(transaction_qty * unit_price),1) OVER (ORDER BY MONTH(transaction_date))) AS sales_difference
+-- FROM coffee_shop_sales
+-- WHERE
+-- 	MONTH(transaction_date) IN (4,5)
+-- GROUP BY
+-- 	MONTH(transaction_date)
+-- ORDER BY
+-- 	MONTH(transaction_date);
+	
+    
